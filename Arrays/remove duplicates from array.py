b3142d55a4e08ce23462ca0de475c18701ce35b0
Eshwar1601 <eshwartejuvankayalapati1@gmail.com>
2025-01-04 22:25:24 +0530

    strivers A to Z

--- Arrays/remove duplicates from array.py
+++ Arrays/remove duplicates from array.py
@@ -1,5 +1,5 @@
 #remove duplicates from array
-def removeDuplicates(self, nums: List[int]) -> int:
+def removeDuplicates(self, nums):
         i=0
         for j in range(1,len(nums)):
             if nums[i]!=nums[j]:
